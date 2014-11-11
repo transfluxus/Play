b5ddf5f4e1342fc4dbfef594c18544f632dd61ec
Ramin S <diskordier@gmail.com>
2014-11-11 10:00:08 +0100

    library commit-1

--- src/play/Play.java
+++ src/play/Play.java
@@ -6,51 +6,120 @@ import processing.core.PApplet;
 import processing.core.PImage;
 import processing.core.PVector;
 
+/**
+ * Play makes stuff easier
+ * 
+ * @author Ramin
+ *
+ */
 public class Play {
 
 	private static PApplet parent;
 
-	public static float ellipseA;
-	public static float ellipseB;
-	public static float circleDia;
-	public static float squareSide;
+	private static float ellipseA;
+	private static float ellipseB;
+	private static float circleDia;
+	private static float squareSide;
 
-	public static HashMap<String, PImage> imgs = new HashMap<>();
+	private static HashMap<String, PImage> imgs = new HashMap<>();
 
-	public Play() {
-	}
-
+	/**
+	 * Initialize Play with this method
+	 * 
+	 * @param ap
+	 *            pass your sketch like usual with 'this'
+	 * @return a Play object(not necessary tho)
+	 */
 	public static Play start(PApplet ap) {
 		parent = ap;
 		return new Play();
 	}
 
+	/**
+	 * A random point in the frame
+	 * 
+	 * @return a random PVector
+	 */
 	public static PVector randPoint() {
 		return new PVector(parent.width, parent.height);
 	}
 
-	public static void ellipse(PVector mid) {
-		ellipse(mid.x, mid.y);
+	/**
+	 * lazy ellipse draw. Uses the previously set a,b
+	 * 
+	 * @param pos
+	 *            position to draw
+	 */
+	public static void ellipse(PVector pos) {
+		ellipse(pos.x, pos.y);
 	}
 
+	/**
+	 * lazy ellipse draw. Uses the previously set a,b
+	 * 
+	 * @param x
+	 *            x-coordinate
+	 * @param y
+	 *            y-coordinate
+	 */
 	public static void ellipse(float x, float y) {
 		parent.ellipse(x, y, ellipseA, ellipseB);
 	}
 
-	public static void ellipse(PVector mid, float a, float b) {
-		ellipse(mid.x, mid.y, a, b);
+	/**
+	 * draws an ellipse Sets a,b for lazy repeat
+	 * 
+	 * @param pos
+	 *            position
+	 * @param a
+	 *            a
+	 * @param b
+	 *            b
+	 */
+	public static void ellipse(PVector pos, float a, float b) {
+		ellipse(pos.x, pos.y, a, b);
 	}
 
+	/**
+	 * Sets a,b for lazy repeat
+	 * 
+	 * @param x
+	 *            x-position
+	 * @param y
+	 *            y-position
+	 * @param a
+	 *            a
+	 * @param b
+	 *            b
+	 */
 	public static void ellipse(float x, float y, float a, float b) {
 		ellipseA = a;
 		ellipseB = b;
 		parent.ellipse(x, y, a, b);
 	}
 
-	public static void ellipseRot(PVector mid, float angle) {
-		ellipseRot(mid.x, mid.y, angle);
+	/**
+	 * Draws a rotated ellipse. Uses the previously defined a,b.
+	 * 
+	 * @param pos
+	 *            ellipse position
+	 * @param angle
+	 *            rotation angle
+	 */
+	public static void ellipseRot(PVector pos, float angle) {
+		ellipseRot(pos.x, pos.y, angle);
 	}
 
+	/**
+	 * Draws a rotated ellipse. Also uses the previously defined a,b.
+	 * 
+	 * @param x
+	 *            x-position
+	 * @param y
+	 *            y-position
+	 * @param angle
+	 *            rotation angle
+	 */
 	public static void ellipseRot(float x, float y, float angle) {
 		parent.pushMatrix();
 		parent.translate(x, y);
@@ -59,10 +128,33 @@ public class Play {
 		parent.popMatrix();
 	}
 
+	/**
+	 * Draws a rotated ellipse.Sets the a,b for lazy repeat
+	 * 
+	 * @param pos
+	 *            ellipse position
+	 * @param angle
+	 *            rotation angle
+	 */
+
 	public static void ellipseRot(PVector mid, float a, float b, float angle) {
 		ellipseRot(mid.x, mid.y, a, b, angle);
 	}
 
+	/**
+	 * Draws a rotated ellipse.Sets the a,b for lazy repeat
+	 * 
+	 * @param x
+	 *            x-position
+	 * @param y
+	 *            y-position
+	 * @param a
+	 *            a
+	 * @param b
+	 *            b
+	 * @param angle
+	 *            rotation angle
+	 */
 	public static void ellipseRot(float x, float y, float a, float b,
 			float angle) {
 		ellipseA = a;
@@ -74,60 +166,166 @@ public class Play {
 		parent.popMatrix();
 	}
 
-	public static void circle(PVector mid, float dia) {
-		circle(mid.x, mid.y, dia);
+	/**
+	 * Draws a circle at the given position. Sets the diameter for lazy repeat
+	 * 
+	 * @param pos
+	 *            position of the circle
+	 * @param dia
+	 *            diameter(or radius,... depends on the ellipseMode)
+	 */
+	public static void circle(PVector pos, float dia) {
+		circle(pos.x, pos.y, dia);
 	}
 
+	/**
+	 * Draws a circle at the given position. Sets the diameter for lazy repeat
+	 * 
+	 * 
+	 * @param x
+	 *            x-position
+	 * @param y
+	 *            y-position
+	 * @param dia
+	 *            diameter(or radius,... depends on the ellipseMode)
+	 */
 	public static void circle(float x, float y, float dia) {
 		circleDia = dia;
 		parent.ellipse(x, y, dia, dia);
 	}
 
-	public static void circle(PVector mid) {
-		circle(mid.x, mid.y);
+	/**
+	 * Draws a circle at the given position. Uses the previously set diameter
+	 * 
+	 * @param pos
+	 *            position
+	 */
+	public static void circle(PVector pos) {
+		circle(pos.x, pos.y);
 	}
 
+	/**
+	 * Draws a circle at the given position. Uses the previously set diameter
+	 * 
+	 * @param x
+	 *            x-position
+	 * @param y
+	 *            y-position
+	 */
 	public static void circle(float x, float y) {
 		parent.ellipse(x, y, circleDia, circleDia);
 	}
 
+	/**
+	 * Draws a square at the given position. Sets the square side length for
+	 * lazy repeat
+	 * 
+	 * @param p
+	 *            The position
+	 * @param side
+	 *            sidelength
+	 */
 	public static void square(PVector p, float side) {
 		square(p.y, p.y, side);
 	}
 
+	/**
+	 * Draws a square at the given position.Sets the square side length for lazy
+	 * repeat
+	 * 
+	 * @param x
+	 *            x-position
+	 * @param y
+	 *            y-position
+	 * @param side
+	 *            sidelength
+	 */
 	public static void square(float x, float y, float side) {
 		squareSide = side;
 		parent.rect(x, y, side, side);
 	}
 
+	/**
+	 * Draws a square at the given position. Uses the previously set sidelength
+	 * 
+	 * @param p
+	 *            position
+	 */
 	public static void square(PVector p) {
 		square(p.x, p.y);
 	}
 
+	/**
+	 * Draws a square at the given position. Uses the previously set sidelength
+	 * 
+	 * @param x
+	 *            x-position
+	 * @param y
+	 *            y-position
+	 */
 	public static void square(float x, float y) {
 		parent.rect(x, y, squareSide, squareSide);
 	}
 
+	/**
+	 * The center of the frame
+	 * 
+	 * @return center as a PVector
+	 */
 	public static PVector center() {
 		return new PVector(parent.width / 2, parent.height / 2);
 	}
 
+	/**
+	 * Draws a line from a to b
+	 * 
+	 * @param a
+	 * @param b
+	 */
 	public static void line(PVector a, PVector b) {
 		parent.line(a.x, a.y, b.x, b.y);
 	}
 
+	/**
+	 * Draws a triangle between a,b and c
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 */
 	public static void triangle(PVector a, PVector b, PVector c) {
 		parent.triangle(a.x, a.y, b.x, b.y, c.x, c.y);
 	}
 
+	/**
+	 * Draws a bezier curve
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @param d
+	 */
 	public static void bezier(PVector a, PVector b, PVector c, PVector d) {
 		parent.bezier(a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y);
 	}
 
+	/**
+	 * draws a quad
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @param d
+	 */
 	public static void quad(PVector a, PVector b, PVector c, PVector d) {
 		parent.quad(a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y);
 	}
 
+	/**
+	 * Returns a random framecorner
+	 * 
+	 * @return
+	 */
 	public static PVector randomCorner() {
 		int p = (int) parent.random(4);
 		switch (p) {
@@ -143,33 +341,80 @@ public class Play {
 		}
 	}
 
-	public static PVector mousePos() {
+	/**
+	 * Returns the location of the mouse as a PVector
+	 * 
+	 * @return mouseposition
+	 */
+	public static PVector mouse() {
 		return new PVector(parent.mouseX, parent.mouseY);
 	}
 
+	/**
+	 * Draws a point
+	 * 
+	 * @param pos
+	 */
 	public static void point(PVector pos) {
 		parent.point(pos.x, pos.y);
 	}
 
+	/**
+	 * Draws a fullframe recangle.Preferably with non-full opacity
+	 * 
+	 * @param color
+	 *            rectangle color
+	 */
 	public static void fade(int color) {
+		parent.pushStyle();
 		parent.fill(color);
 		parent.rect(-10, -10, parent.width + 10, parent.height + 10);
+		parent.popStyle();
 	}
 
-	public static void fade(int color, int strength) {
+	/**
+	 * Draws a fullframe recangle.
+	 * 
+	 * @param color
+	 *            color
+	 * @param opactity
+	 *            opactity
+	 */
+	public static void fade(int color, int opactity) {
 		parent.fill(parent.red(color), parent.green(color), parent.blue(color),
-				strength);
+				opactity);
 		parent.rect(-10, -10, parent.width + 10, parent.height + 10);
 	}
 
+	/**
+	 * A frame in square size
+	 * 
+	 * @param sqSize
+	 *            width&height of the frame
+	 */
 	public static void size(int sqSize) {
 		parent.size(sqSize, sqSize);
 	}
 
+	/**
+	 * Fast PVector creation
+	 * 
+	 * @param x
+	 *            x-coordinate
+	 * @param y
+	 *            y-coordinate
+	 * @return a PVector at x,y
+	 */
 	public static PVector vec(float x, float y) {
 		return new PVector(x, y);
 	}
 
+	/**
+	 * Draws a closing PShape with the given points
+	 * 
+	 * @param ps
+	 *            Shapepoints
+	 */
 	public static void shape(PVector... ps) {
 		parent.beginShape();
 		for (PVector p : ps)
@@ -177,20 +422,71 @@ public class Play {
 		parent.endShape(PApplet.CLOSE);
 	}
 
+	/**
+	 * A shape-vertex
+	 * 
+	 * @param p
+	 */
+	public static void vertex(PVector p) {
+		parent.vertex(p.x, p.y);
+	}
+
+	/**
+	 * Lerp between two vectors
+	 * 
+	 * @param a
+	 *            vector a
+	 * @param b
+	 *            vector b
+	 * @param amt
+	 *            amt
+	 * @return new PVector in between a and b
+	 */
 	public static PVector lerp(PVector a, PVector b, float amt) {
 		return lerp(a.x, a.y, b.x, b.y, amt);
 	}
 
+	/**
+	 * Lerp between 2 locations
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @param amt
+	 * @return A PVector in between x1,y2 & x2,y2
+	 */
 	public static PVector lerp(float x1, float y1, float x2, float y2, float amt) {
 		return new PVector(PApplet.lerp(x1, x2, amt), PApplet.lerp(y2, y2, amt));
 	}
 
+	/**
+	 * Creates a PVector from an angle and magnitude
+	 * 
+	 * @param ang
+	 *            angle
+	 * @param length
+	 *            length(magnitude)
+	 * @return a brand new PVector
+	 */
 	public static PVector fromAngle(float ang, float length) {
 		PVector p = PVector.fromAngle(ang);
 		p.mult(length);
 		return p;
 	}
 
+	/**
+	 * Creates a PVector from an angle and magnitude and a given reference point
+	 * so its like: rel.x+cos(ang)*length , rel.y+sin(ang)*length
+	 * 
+	 * @param rel
+	 *            relative point
+	 * @param ang
+	 *            angle
+	 * @param length
+	 *            length (magnitude)
+	 * @return a brand new PVectpr
+	 */
 	public static PVector fromAngle(PVector rel, float ang, float length) {
 		PVector p = PVector.fromAngle(ang);
 		p.mult(length);
@@ -198,6 +494,15 @@ public class Play {
 		return p;
 	}
 
+	/**
+	 * Gives the angle going from one vector to another
+	 * 
+	 * @param from
+	 *            starting vector
+	 * @param to
+	 *            ending vector
+	 * @return an angle
+	 */
 	public static float angleTo(PVector from, PVector to) {
 		float a = PVector
 				.angleBetween(new PVector(1, 0), PVector.sub(to, from));
@@ -206,24 +511,67 @@ public class Play {
 		return a;
 	}
 
+	/**
+	 * Gives a random point in between two points
+	 * 
+	 * @param a
+	 * @param b
+	 * @return a PVector between a and b
+	 */
 	public static PVector randomLineP(PVector a, PVector b) {
 		return randomLineP(a.x, a.y, b.x, b.y);
 	}
 
+	/**
+	 * Gives a random point in between two points
+	 * 
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return a random point between x1,y1 and x2,y2
+	 */
 	public static PVector randomLineP(float x1, float y1, float x2, float y2) {
 		return lerp(x1, y1, x2, y2, random());
 	}
 
+	/**
+	 * Gives a random point in a rectangle
+	 * 
+	 * @param p
+	 *            rect position
+	 * @param width
+	 * @param height
+	 * @return a PVector in the rectangle
+	 */
 	public static PVector randomRectP(PVector p, float width, float height) {
 		return randomRectP(p.x, p.y, width, height);
 	}
 
+	/**
+	 * Gives a random point in a rectangle
+	 * 
+	 * 
+	 * @param x
+	 *            x-pos of the rectangle
+	 * @param y
+	 *            y-pos of the rectangle
+	 * @param width
+	 * @param height
+	 * @return a PVector in the rectangle
+	 */
 	public static PVector randomRectP(float x, float y, float width,
 			float height) {
 		return new PVector(parent.random(x, x + width), parent.random(y, y
 				+ height));
 	}
 
+	/**
+	 * A random number between 0 and 1
+	 * 
+	 * @return
+	 */
 	public static float random() {
 		return parent.random(1);
 	}
@@ -235,12 +583,26 @@ public class Play {
 			return imgs.put(imgName, parent.loadImage(imgName));
 	}
 
+	/**
+	 * draws an image. no loadImage required
+	 * 
+	 * @param imgName
+	 * @param pos
+	 * @param width
+	 * @param height
+	 */
 	public static void image(String imgName, PVector pos, float width,
 			float height) {
 		PImage img = getImage(imgName);
 		parent.image(img, pos.x, pos.y, width, height);
 	}
 
+	/**
+	 * draws an image. no loadImage required
+	 * 
+	 * @param imgName
+	 * @param pos
+	 */
 	public static void image(String imgName, PVector pos) {
 		PImage img = getImage(imgName);
 		parent.image(img, pos.x, pos.y);
